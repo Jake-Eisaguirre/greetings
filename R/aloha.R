d56b0cf9f9261cedd0eadc90d3d601f7426b4843
Ryanmunnikhuis <rmunnikhuis@bren.ucsb.edu>
2021-08-23 14:38:01 -0700

    Made changes.

--- R/aloha.R
+++ R/aloha.R
@@ -8,7 +8,9 @@
 #' @export
 #'
 #' @examples say_aloha("Allison")
-say_aloha <- function(name) {
+library(emo)
+library(crayon)
+say_aloha <- function(name, color) {
 
   message <- paste("Aloha,",
                    name,
@@ -16,5 +18,15 @@ say_aloha <- function(name) {
                    emo::ji("sunny"),
                    emo::ji("ocean"))
 
-  cat(crayon::bgGreen(message))
+  if (color == "green") {
+    cat(crayon::bgGreen(message))
+  } else if(color == "blue") {
+    cat(crayon::bgBlue(message))
+  } else if (color == "red") {
+    cat(crayon::bgRed(message))
+  } else if (color == "cyan") {
+    cat(crayon::bgCyan(message))
 }
+}
+
+say_aloha("Ryan", "blue")
